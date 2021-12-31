40bfeab088064b0ed895a80438be947e24130156
FLAK-ZOSO <83119697+FLAK-ZOSO@users.noreply.github.com>
2022-01-01 00:53:52 +0100

    Add files via upload

---
+++ source.py
@@ -0,0 +1,15 @@
+#usr/bin/env Python3
+import os
+
+
+def parse(path: str) -> list[str]:
+    with open(path, 'r') as commands:
+        lines = commands.read().split('\n')
+    return lines
+
+
+def run(path: str) -> None:
+    commands = 'cd D:\\Python\Python\Prompt | main.py'
+    for line in parse(path):
+        commands += f' | {line}'
+    os.system(commands)
